740010a09fdfdb41285b2826527cdba2ea83ce7c
21Horza <im.shoo@yandex.ru>
2022-04-30 22:14:23 +0800

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -50139,7 +50139,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51245" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61913" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
